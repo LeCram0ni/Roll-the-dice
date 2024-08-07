1ce3d62fd0e589b777adea16b6928b46e10bedea
marcel <15247414+LeCram0ni@users.noreply.github.com>
2024-08-07 11:16:55 +0200

    first

---
+++ script.js
@@ -0,0 +1,25 @@
+function updateDice() {
+  let diceNumber = document.getElementById("dice").value;
+  renderDice(diceNumber);
+}
+
+function renderDice(diceNumber) {
+  clear();
+  for (let i = 0; i < diceNumber; i++) {
+    let diceInstance = new Dice(randomEye());
+    console.log(diceInstance);
+    document.getElementById("output").innerHTML += `<div id="dice-instance">${diceInstance.eye}</div>`;
+  }
+}
+
+function Dice(eye) {
+  this.eye = eye;
+}
+
+function randomEye() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function clear(){
+    document.getElementById("output").innerHTML = ``;
+}
